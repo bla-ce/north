454401a1dcbb702035ea4d2939aeaa88ece5cb33
bla-ce <contact.charaud@gmail.com>
2024-04-28 15:03:01 +1000

    cat function to read file

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,45 @@
 #include <cassert>
 #include <cstring>
+#include <fstream>
 #include <iostream>
 
-void usage(const char *program) {
-  std::cout << "Usage: " << program << " <SUBCOMMAND>\n";
+void usage() {
+  std::cout << "Usage: ./north <SUBCOMMAND>\n";
   std::cout << "SUBCOMMANDS:\n";
   std::cout << "    interpret <program>   interpret file\n";
   std::cout << "    compile   <program>    compile file.\n";
+}
+
+std::ifstream read_file(const char *path) {
+  std::ifstream file;
+
+  file.open(path, std::ifstream::in);
+  
+  assert(file.good() && "ERROR: failed to open file");
+
+  return file;
+}
+
+void cat(const char *path) {
+  std::ifstream file{ read_file(path) };
+
+  char c = file.get();
+
+  while(file.good()) {
+    std::cout << c;
+    c = file.get();
+  }
+    
+  file.close();
+}
+
+void interpret(char *argv) {
+  if(argv == nullptr) {
+    usage();
+    std::cerr << "ERROR: no file is provided";
+  }
+
+  cat(argv);
 }
 
 int main(int argc, char *argv[]) {
@@ -14,8 +47,8 @@ int main(int argc, char *argv[]) {
   const char *program{*argv++};
 
   if (argc < 2) {
-    usage(program);
-    std::cerr << "ERROR: not enough arguments\n";
+    usage();
+    std::cerr << "ERROR: no subcommand is provided\n";
     return -1;
   }
 
@@ -23,11 +56,11 @@ int main(int argc, char *argv[]) {
   const char *subcommand{*argv};
 
   if (strcmp(*argv, "interpret") == 0) {
-    assert(false && "TODO: implement interpreter");
+    interpret(*++argv);
   } else if (strcmp(*argv, "compile") == 0) {
     assert(false && "TODO: implement compiler");
   } else {
-    usage(program);
+    usage();
     std::cerr << "ERROR: unknown subcommand " << subcommand << '\n';
     return (-1);
   }
