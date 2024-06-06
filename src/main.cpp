4bde2a8f2c81a49084180ded0e2d711d8e44ed20
bla-ce <contact.charaud@gmail.com>
2024-06-06 15:09:21 +1000

    split code by line for error reporting

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <cstring>
 #include <ctime>
 #include <fstream>
+#include <sstream>
 #include <iostream>
 #include <stack>
 #include <string>
@@ -13,6 +14,14 @@
 
 #include "../includes/assembly.h"
 #include "../includes/system.h"
+
+class Token {
+  public:
+    int row{};
+    int col{};
+    std::string value{};
+  private:
+};
 
 bool g_run_mode{false};
 char *output{};
@@ -36,24 +45,36 @@ std::ifstream read_file(const char *path) {
   return file;
 }
 
-std::vector<std::string> tokenize(const char *path) {
-  std::vector<std::string> tokens;
-
-  std::ifstream file{read_file(path)};
+std::vector<std::string> split_by_line(const char *path) {
+  std::vector<std::string> lines{};
+  std::ifstream file{path};
 
   if (!file.is_open()) {
     std::cerr << "Error opening file: " << path << std::endl;
-    return tokens;
+    return lines;
   }
 
-  std::string token;
-  while (file >> token) {
-    tokens.push_back(token);
+  std::string line;
+  while (std::getline(file, line)) { 
+    lines.push_back(line);
   }
 
   file.close();
 
-  return tokens;
+  return lines;
+}
+
+void tokenize(const std::string line, std::vector<Token> &tokens, int n_line) {
+  std::stringstream s(line);
+  std::string s_token;
+
+  while (s >> s_token) {
+    Token token;
+    token.row = n_line;
+    token.value = s_token; 
+
+    tokens.push_back(token);
+  }
 }
 
 void compile(char *argv, std::string output_filename) {
@@ -73,7 +94,14 @@ void compile(char *argv, std::string output_filename) {
   output_file << base_asm();
   output_file << helpers();
 
-  std::vector<std::string> tokens{tokenize(argv)};
+  std::vector<std::string> lines{split_by_line(argv)};
+  std::vector<Token> tokens{};
+
+  int n_line{};
+  for(const auto &line : lines) {
+    n_line++;
+    tokenize(line, tokens, n_line);
+  }
 
   bool comments { false };
   bool string_literals { false };
@@ -90,11 +118,12 @@ void compile(char *argv, std::string output_filename) {
   int n_loops{1};
   std::stack<int> stack_loop{};
 
-  for (const std::string &token : tokens) {
+  for (const Token &token : tokens) {
     n_tokens++;
+    std::string s_token{token.value};
 
     if(comments) { 
-      if(token[token.length()-1] == ')') {
+      if(s_token[s_token.length()-1] == ')') {
         comments = false;
       } 
 
@@ -102,8 +131,8 @@ void compile(char *argv, std::string output_filename) {
     }
 
     if(string_literals) { 
-      if(token[token.length()-1] == '"') {
-        str += " " + token;
+      if(s_token[s_token.length()-1] == '"') {
+        str += " " + s_token;
 
         string_literals = false;
         strings.push_back(str);
@@ -114,232 +143,232 @@ void compile(char *argv, std::string output_filename) {
 
         str = "";
       } else {
-        str += " " + token;
+        str += " " + s_token;
       }
 
       continue; 
     }
 
-    if (token == "+") {
+    if (s_token == "+") {
       output_file << plus_assembly();
       continue;
     }
 
-    if (token == "-") {
+    if (s_token == "-") {
       output_file << minus_assembly();
       continue;
     }
 
-    if (token == ".") {
+    if (s_token == ".") {
       output_file << dump_assembly();
       continue;
     }
 
-    if (token == "PRINT") {
+    if (s_token == "PRINT") {
       output_file << print_assembly();
       continue;
     }
 
-    if (token == "EMIT") {
+    if (s_token == "EMIT") {
       output_file << emit_assembly();
       continue;
     }
 
-    if (token == "=") {
+    if (s_token == "=") {
       output_file << equal_assembly();
       continue;
     }
 
-    if (token == "*") {
+    if (s_token == "*") {
       output_file << mult_assembly();
       continue;
     }
 
-    if (token == "DUP") {
+    if (s_token == "DUP") {
       output_file << dup_assembly();
       continue;
     }
 
-    if (token == "2DUP") {
+    if (s_token == "2DUP") {
       output_file << two_dup_assembly();
       continue;
     }
 
-    if (token == "/") {
+    if (s_token == "/") {
       output_file << div_assembly();
       continue;
     }
 
-    if (token == "MOD") {
+    if (s_token == "MOD") {
       output_file << mod_assembly();
       continue;
     }
 
-    if (token == "/MOD") {
+    if (s_token == "/MOD") {
       output_file << div_mod_assembly();
       continue;
     }
 
-    if (token == "SWAP") {
+    if (s_token == "SWAP") {
       output_file << swap_assembly();
       continue;
     }
 
-    if (token == "DROP") {
+    if (s_token == "DROP") {
       output_file << drop_assembly();
       continue;
     }
 
-    if (token == "2DROP") { 
+    if (s_token == "2DROP") { 
       output_file << two_drop_assembly();
       continue;
     }
 
-    if (token == "OVER") {
+    if (s_token == "OVER") {
       output_file << over_assembly();
       continue;
     }
 
-    if (token == "ROT") {
+    if (s_token == "ROT") {
       output_file << rot_assembly();
       continue;
     }
 
-    if (token == "<") {
+    if (s_token == "<") {
       output_file << less_assembly();
       continue;
     }
 
-    if (token == ">") {
+    if (s_token == ">") {
       output_file << greater_assembly();
       continue;
     }
 
-    if (token == "0=") {
+    if (s_token == "0=") {
       output_file << is_zero_assembly();
       continue;
     }
 
-    if (token == "0<") {
+    if (s_token == "0<") {
       output_file << is_negative_assembly();
       continue;
     }
 
-    if (token == "0>") {
+    if (s_token == "0>") {
       output_file << is_positive_assembly();
       continue;
     }    
 
-    if (token == "NOT") {
+    if (s_token == "NOT") {
       output_file << not_assembly();
       continue;
     }
 
-    if (token == "?DUP") {
+    if (s_token == "?DUP") {
       output_file << dup_non_zero_assembly(n_conditions);
       n_conditions++;
       continue;
     }
 
-    if (token == "IF") {
+    if (s_token == "IF") {
       output_file << if_assembly(n_conditions);
       stack_condition.push(n_conditions);
       n_conditions++;
       continue;
     }
 
-    if (token == "ELSE") {
+    if (s_token == "ELSE") {
       output_file << else_assembly(n_conditions-1);
       continue;
     }
 
-    if (token == "THEN") {
+    if (s_token == "THEN") {
       output_file << then_assembly( stack_condition.top() );
       stack_condition.pop();
       continue;
     }
 
-    if (token == "AND") {
+    if (s_token == "AND") {
       output_file << and_assembly();
       continue;
     }
 
-    if (token == "OR") {
+    if (s_token == "OR") {
       output_file << or_assembly();
       continue;
     }
 
-    if (token == "1+") {
+    if (s_token == "1+") {
       output_file << one_plus_assembly();
       continue;
     }
 
-    if (token == "1-") {
+    if (s_token == "1-") {
       output_file << one_minus_assembly();
       continue;
     }
     
-    if (token == "2+") {
+    if (s_token == "2+") {
       output_file << two_plus_assembly();
       continue;
     }
 
-    if (token == "2-") {
+    if (s_token == "2-") {
       output_file << two_minus_assembly();
       continue;
     }
 
-    if (token == "2*") {
+    if (s_token == "2*") {
       output_file << two_mult_assembly();
       continue;
     }
 
-    if (token == "2/") {
+    if (s_token == "2/") {
       output_file << two_div_assembly();
       continue;
     }
 
-    if (token == "MAX") {
+    if (s_token == "MAX") {
       output_file << max_assembly();
       continue;
     }
 
-    if (token == "MIN") {
+    if (s_token == "MIN") {
       output_file << min_assembly();
       continue;
     }
 
-    if (token == "*/") {
+    if (s_token == "*/") {
       output_file << div_mult_assembly();
       continue;
     }
 
-    if (token == ">R") {
+    if (s_token == ">R") {
       output_file << push_to_ret_stack();
       continue;
     }    
 
-    if (token == "R>") {
+    if (s_token == "R>") {
       output_file << push_from_ret_stack(); // Pop value from ret stack and push to param stack
       continue;
     }
 
-    if (token == "I") {
+    if (s_token == "I") {
       output_file << copy_top_ret_stack(); 
       continue;
     }
 
-    if (token == "I'") {
+    if (s_token == "I'") {
       output_file << copy_second_ret_stack(); 
       continue;
     }
 
-    if (token == "J") {
+    if (s_token == "J") {
       output_file << copy_third_ret_stack(); 
       continue;
     }
 
-    if (token == "DO") {
+    if (s_token == "DO") {
       output_file << push_to_ret_stack();
       output_file << push_to_ret_stack();
 
@@ -350,40 +379,40 @@ void compile(char *argv, std::string output_filename) {
       continue;
     }
 
-    if (token == "LOOP") {
+    if (s_token == "LOOP") {
       output_file << loop_assembly( stack_loop.top() ); 
       stack_loop.pop();
       continue;
     }
 
-    if (token == "+LOOP") {
+    if (s_token == "+LOOP") {
       output_file << plus_loop_assembly( stack_loop.top() ); 
       stack_loop.pop();
       continue;
     }
 
-    if (token == "CR") {
+    if (s_token == "CR") {
       output_file << carriage_return(); 
       continue;
     }
 
-    if (token == "SPACES") {
+    if (s_token == "SPACES") {
       output_file << print_space(); 
       continue;
     }
 
-    if (token == "BEGIN") {
+    if (s_token == "BEGIN") {
       output_file << begin_assembly(); 
       continue;
     }
 
-    if (token == "UNTIL") {
+    if (s_token == "UNTIL") {
       output_file << until_assembly(); 
       continue;
     }
 
-    if (token == "(") {
-      if(token[token.length()-1] == ')') {
+    if (s_token == "(") {
+      if(s_token[s_token.length()-1] == ')') {
         continue;
       }
 
@@ -391,14 +420,14 @@ void compile(char *argv, std::string output_filename) {
       continue;
     }
 
-    if (token[0] == '"') {
+    if (s_token[0] == '"') {
       //TODO: better way to do that ?
-      str += token;
+      str += s_token;
       string_literals = true;
 
-      if(token[token.length()-1] == '"') {
+      if(s_token[s_token.length()-1] == '"') {
         string_literals = false;
-        strings.push_back(token);
+        strings.push_back(s_token);
 
         output_file << push_string_assembly(count_strings);
 
@@ -410,11 +439,11 @@ void compile(char *argv, std::string output_filename) {
       continue;
     }
     
-    if (std::all_of(token.begin(), token.end(),
+    if (std::all_of(s_token.begin(), s_token.end(),
                     [](unsigned char c) { return std::isdigit(c); })) {
-      output_file << push_assembly(token);
+      output_file << push_assembly(s_token);
     } else {
-      std::cerr << "ERROR at token " + std::to_string(n_tokens) + ": unknown token " << token << '\n';
+      std::cerr << argv << ':' << std::to_string(token.row) << ": error: unknown token " << s_token << '\n';
       exit(-1);
     }
   }
